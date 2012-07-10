2e9b6d2b2fdaf4c168456d36a257b5fecf5a7289
Ben Woznicki <benwoznicki@gmail.com>
2012-07-10 13:54:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSpike::Application.config.secret_token = 'da6ba2ca574ac6b8b8a7b03c4273c9000ac1c65cb529d8758083bef92283d8be46483cde7eff50220060a8aa1334de4c732a7339a42a674b7cd43702241c4eab'
